2ea5f5a6ab4aa4bb29b2831806d5bad532c18569
ElisDN <mail@elisdn.ru>
2018-10-06 16:15:42 +0300

    Installed font-awesome

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -32,6 +32,7 @@
 <style lang="scss">
   @import "~bootstrap/dist/css/bootstrap.css";
   @import "~bootstrap-vue/dist/bootstrap-vue.css";
+  @import "~@fortawesome/fontawesome-free/css/all.css";
 
   body {
     min-height: 100vh;
