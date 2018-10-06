711c7b8d6e22426f4c72965a56ec5d0574aaf5ea
ElisDN <mail@elisdn.ru>
2018-10-06 19:21:28 +0300

    Added api dotenv

--- api/config/config.php
+++ api/config/config.php
@@ -7,6 +7,7 @@ use Api\Http\Action;
 return [
     'settings' => [
         'addContentLengthHeader' => false,
+        'displayErrorDetails' => (bool)getenv('API_DEBUG'),
     ],
 
     Action\HomeAction::class => function () {
--- api/public/index.php
+++ api/public/index.php
@@ -3,9 +3,14 @@
 declare(strict_types=1);
 
 use Api\Http\Action;
+use Symfony\Component\Dotenv\Dotenv;
 
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
+
+if (file_exists('.env')) {
+    (new Dotenv())->load('.env');
+}
 
 $config = require 'config/config.php';
 $container = new \Slim\Container($config);
