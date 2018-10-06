401dce3d21813b58bd42a20687079ed2fa6c11a3
ElisDN <mail@elisdn.ru>
2018-10-06 15:05:12 +0300

    Added environment variable

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -6,7 +6,7 @@ import axios from 'axios'
 
 Vue.config.productionTip = false;
 
-axios.defaults.baseURL = 'http://127.0.0.1:8081';
+axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 
 new Vue({
   router,
