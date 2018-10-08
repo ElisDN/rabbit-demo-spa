2af3861a913710495b9c739f21364e70772e6667
ElisDN <mail@elisdn.ru>
2018-10-08 18:42:23 +0300

    Installed Doctrine

--- api/config/config.php
+++ api/config/config.php
@@ -3,6 +3,11 @@
 declare(strict_types=1);
 
 use Api\Http\Action;
+use Doctrine\Common\Cache\FilesystemCache;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Setup;
+use Psr\Container\ContainerInterface;
 
 return [
     'settings' => [
@@ -13,4 +18,32 @@ return [
     Action\HomeAction::class => function () {
         return new Action\HomeAction();
     },
+
+    EntityManagerInterface::class => function (ContainerInterface $container) {
+        $params = $container['config']['doctrine'];
+        $config = Setup::createAnnotationMetadataConfiguration(
+            $params['metadata_dirs'],
+            $params['dev_mode'],
+            $params['cache_dir'],
+            new FilesystemCache(
+                $params['cache_dir']
+            ),
+            false
+        );
+        return EntityManager::create(
+            $params['connection'],
+            $config
+        );
+    },
+
+    'config' => [
+        'doctrine' => [
+            'dev_mode' => true,
+            'cache_dir' => 'var/cache/doctrine',
+            'metadata_dirs' => ['src/Model/User/Entity'],
+            'connection' => [
+                'url' => getenv('API_DB_URL'),
+            ],
+        ],
+    ],
 ];
