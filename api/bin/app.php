91c94af46c298965885b5ddfbd1066b30b1d396e
ElisDN <mail@elisdn.ru>
2018-10-08 19:18:04 +0300

    Installed Doctrine migrations

--- api/bin/app.php
+++ api/bin/app.php
@@ -3,6 +3,9 @@
 
 declare(strict_types=1);
 
+use Doctrine\DBAL\Migrations\Tools\Console\Helper\ConfigurationHelper;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Tools\Console\Helper\EntityManagerHelper;
 use Symfony\Component\Console\Application;
 use Symfony\Component\Dotenv\Dotenv;
 
@@ -21,9 +24,17 @@ $container = require 'config/container.php';
 $cli = new Application('Application console');
 
 $entityManager = $container->get(EntityManagerInterface::class);
+$connection = $entityManager->getConnection();
+
+$configuration = new Doctrine\DBAL\Migrations\Configuration\Configuration($connection);
+$configuration->setMigrationsDirectory('src/Data/Migration');
+$configuration->setMigrationsNamespace('Api\Data\Migration');
+
 $cli->getHelperSet()->set(new EntityManagerHelper($entityManager), 'em');
+$cli->getHelperSet()->set(new ConfigurationHelper($connection, $configuration), 'configuration');
 
 Doctrine\ORM\Tools\Console\ConsoleRunner::addCommands($cli);
+Doctrine\DBAL\Migrations\Tools\Console\ConsoleRunner::addCommands($cli);
 
 $commands = $container->get('config')['console']['commands'];
 foreach ($commands as $command) {
