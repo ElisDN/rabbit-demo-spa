932cf352e970540a1b3cda12afae8346473c6bd7
ElisDN <mail@elisdn.ru>
2018-10-17 18:50:45 +0300

    Removed kafka

--- api/config/common/console.php
+++ api/config/common/console.php
@@ -1,25 +1,10 @@
 <?php
 
 use Api\Console\Command;
-use Kafka\ConsumerConfig;
-use Kafka\Producer;
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use Psr\Container\ContainerInterface;
-use Psr\Log\LoggerInterface;
 
 return [
-    Command\Kafka\ProduceCommand::class => function (ContainerInterface $container) {
-        return new Command\Kafka\ProduceCommand(
-            $container->get(Producer::class)
-        );
-    },
-    Command\Kafka\ConsumeCommand::class => function (ContainerInterface $container) {
-        return new Command\Kafka\ConsumeCommand(
-            $container->get(LoggerInterface::class),
-            $container->get(ConsumerConfig::class)
-        );
-    },
-
     Command\Amqp\ProduceCommand::class => function (ContainerInterface $container) {
         return new Command\Amqp\ProduceCommand(
             $container->get(AMQPStreamConnection::class)
@@ -34,9 +19,6 @@ return [
     'config' => [
         'console' => [
             'commands' => [
-                Command\Kafka\ProduceCommand::class,
-                Command\Kafka\ConsumeCommand::class,
-
                 Command\Amqp\ProduceCommand::class,
                 Command\Amqp\ConsumeCommand::class,
             ],
